9cc3ad3f43d0ed94188c37ebcc5ef1553e51cbc6
Sally <hossamhassanahmed5@gmail.com>
2021-05-11 01:05:41 +0200

    [ADD] add product name mosule

---
+++ n2d_product_name/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+
+{
+    "name": "N2d Product Name",
+    "version": "1.0",
+    "author": "Hossam Hassan",
+    "license": "AGPL-3",
+    "depends": ['product', 'sale_management', 'purchase'],
+    "data": [],
+}
